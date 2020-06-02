384ce220345b7e5ed0f1d5ff932e126cf387de10
kimmy100b <kimmy100b@gmail.com>
2020-06-03 01:39:42 +0900

    Saving the User Name commit

--- greeting.js
+++ greeting.js
@@ -1,12 +1,28 @@
 const form = document.querySelector(".js-form"),
-    greeting = document.querySelector(".js-greeing"),
+    greeting = document.querySelector(".js-greeting"),
     input = form.querySelector("input");
 
 const USER_LS = "currentUser",
-    SHOWING_CN = "showing";
+    SHOWING_CN = "showing"; //CN : className, LS : Local Storage
+
+function saveName(text){
+    localStorage.setItem(USER_LS,text);
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+    const currentValue = input.value;
+    paintGreeting(currentValue);
+    saveName(currentValue);
+}
+
+function askForName(){
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit",handleSubmit);
+}
 
 function paintGreeting(text){
-    form.classList
+    form.classList.remove(SHOWING_CN);
     greeting.classList.add(SHOWING_CN);
     greeting.innerText = `Hello ${text}`;
 }
@@ -15,7 +31,7 @@ function localName(){
     const currentUser = localStorage.getItem(USER_LS);
     if(currentUser === null){
         // user is not
-
+        askForName();
 
     } else{
         // user is
@@ -25,8 +41,7 @@ function localName(){
 
     
 function init() {
-  
-
+  localName();
 }
 
 init();

6c00b8d9acb61ec77fc27de90beada84abf202c0
kimmy100b <kimmy100b@gmail.com>
2020-06-08 20:08:07 +0900

    날씨 받아오기 성공

--- weather.js
+++ weather.js
@@ -1,5 +1,18 @@
+const weather = document.querySelector(".js-weather");
+
 const API_KEY = "d505b446bcb6adeee5106e6e446fcd33"
 const COORDS = 'coords';
+
+function getWeather(lat, lon){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+    ).then(function(response){
+        return response.json();
+    }).then(function(json){
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerText = `${temperature} @ ${place}`
+    });
+}
 
 function saveCoords(coordsObj){
     //localStage에 저장
@@ -15,6 +28,7 @@ function handleGeoSucces(position){
     };
 
     saveCoords(coordsObj);
+    getWeather(latitude, longitude);
 }
 
 function handleGeoError(){
@@ -27,11 +41,13 @@ function askForCoords(){
 }
 
 function loadCoords(){
-    const loadedCords = localStorage.getItem(COORDS);
-    if(loadedCords === null){
+    const loadedCoords = localStorage.getItem(COORDS);
+    if(loadedCoords === null){
         askForCoords();
     } else{
-        //getWeather
+        //만약, LS에 정보가 있다면
+        const parseCoords = JSON.parse(loadedCoords);
+        getWeather(parseCoords.latitude, parseCoords.longitude);
     }
 }
 
